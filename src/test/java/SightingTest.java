ed501582c3506dca278b0d5c84350243f26c13c1
Belyse Uwambayinema <ubelyse1@gmail.com>
2020-12-02 09:03:16 +0200

    added all test method

--- src/test/java/SightingTest.java
+++ src/test/java/SightingTest.java
@@ -11,4 +11,57 @@ public class SightingTest {
         Sighting testSighting = new Sighting("zone_A", "national park");
         assertEquals(true, testSighting instanceof Sighting);
     }
+
+    @Test
+    public void getLocation_sighting_A() {
+        Sighting testSightingObj = new Sighting("zone_A", "national park");
+        assertEquals("zone_A", testSightingObj.getLocation());
+    }
+
+    @Test
+    public void getRanger_sighting_String() {
+        Sighting testSightingObj = new Sighting("zone_A", "national park");
+        assertEquals("national park", testSightingObj.getRanger());
+    }
+
+    @Test
+    public void equals_returnsTrueIfAreSame_true() {
+        Sighting firstSighting = new Sighting("zone_A", "national park");
+        Sighting anotherSighting = new Sighting("zone_A", "national park");
+        assertTrue(firstSighting.equals(anotherSighting));
+    }
+
+    @Test
+    public void save_insertsIntoDatabase_Sightings() {
+        Sighting testSightingObj = new Sighting("zone_A", "national park");
+        testSightingObj.save();
+        assertTrue(Sighting.all().get(0).equals(testSightingObj));
+    }
+
+    @Test
+    public void all_returnsAllInstances_true() {
+        Sighting firstSighting = new Sighting("zone_A", "national park");
+        firstSighting.save();
+        Sighting secondSighting = new Sighting("zone_A", "national park");
+        secondSighting.save();
+        assertEquals(true, Sighting.all().get(0).equals(firstSighting));
+        assertEquals(true, Sighting.all().get(1).equals(secondSighting));
+    }
+
+    @Test
+    public void save_assignsIdToSighting() {
+        Sighting testSightingObj = new Sighting("zone_A", "national park");
+        testSightingObj.save();
+        Sighting savedSighting = Sighting.all().get(0);
+        assertEquals(testSightingObj.getId(), savedSighting.getId());
+    }
+
+    @Test
+    public void find_returnsSightingWithSameId_secondSighting() {
+        Sighting firstSighting = new Sighting("zone_A", "national park");
+        firstSighting.save();
+        Sighting secondSighting = new Sighting("zone_A", "national park");
+        secondSighting.save();
+        assertEquals(Sighting.find(secondSighting.getId()), secondSighting);
+    }
 }
