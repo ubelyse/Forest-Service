d65b277ede922ed7c20480b12d3a985f58f69c93
Belyse Uwambayinema <ubelyse1@gmail.com>
2020-12-02 08:26:12 +0200

    added the db class

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,2 +1,6 @@
+import org.sql2o.*;
+
 public class DB {
+    //public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "moringa", "@qwerty");
+    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-174-129-253-86.compute-1.amazonaws.com:5432/d5tkf3dsmrb98s", "tgrzzhuriljlfk", "85d7f9f13c4f92cdcb17bbea55a2d4f2439a4ee39cc3fdb00bff8cee5a4d1254");
 }
