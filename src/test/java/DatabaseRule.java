d1aa2617dee52dc6fa64c124c9361467e8ed5045
Belyse Uwambayinema <ubelyse1@gmail.com>
2020-12-02 08:49:35 +0200

    added the dbrule class

--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -7,4 +7,15 @@ public class DatabaseRule extends ExternalResource {
     protected void before() {
         DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "moringa", "@qwerty");
     }
+
+    @Override
+    protected void after() {
+        try(Connection con = DB.sql2o.open()) {
+            String deleteSightingQuery = "DELETE FROM sightings *;";
+            String deleteAnimalQuery = "DELETE FROM animals *;";
+            con.createQuery(deleteSightingQuery).executeUpdate();
+            con.createQuery(deleteAnimalQuery).executeUpdate();
+
+        }
+    }
 }
